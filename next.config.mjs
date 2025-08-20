fee3e17c9fc0aab7378bb2351d2d3fd59576028b
shreyarai21 <shreyarai2102@gmail.com>
2025-08-20 16:57:10 +0000

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "cuddly-space-winner-g47p66jg7g7g2vp7r-3000.app.github.dev"
+      ]
+    }
+  }
 }
 
 export default nextConfig
